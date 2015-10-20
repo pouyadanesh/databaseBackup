8bd194284f3714236d0964b734a53fb5571f94e4
pouyadanesh <pouyadanesh90@gmail.com>
2015-10-20 19:48:02 +0330

    Update Echo.java

--- src/Echo.java
+++ src/Echo.java
@@ -7,6 +7,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.Environment;
+
+import java.io.*;
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -22,8 +25,46 @@ public class Echo extends CordovaPlugin {
         return false;
     }
 
+    private void copyFile(){
+
+        final String inFileName = "/data/data/me.app.id/app_databases/cacheF147.db";
+        try{
+
+
+            File dbFile = new File(inFileName);
+
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            String outFileName = Environment.getExternalStorageDirectory()+"/databasetahani_copy.db";
+
+            // Open the empty db as the output stream
+            OutputStream output = new FileOutputStream(outFileName);
+
+            // Transfer bytes from the inputfile to the outputfile
+            byte[] buffer = new byte[1024];
+            int length;
+            try{
+
+
+                while ((length = fis.read(buffer))>0){
+                    output.write(buffer, 0, length);
+                }
+
+                // Close the streams
+                output.flush();
+                output.close();
+                fis.close();
+            }catch(FileNotFoundException e){
+                System.out.println("this is file not found exception");
+            }
+        } catch(IOException e){
+            System.out.println("this is an io exception");
+        }
+    }
+
     private void echo(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
+            this.copyFile();
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
